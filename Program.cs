1f77401017034423241bb692edeb4f39f1e82da8
kent2pas <kentcejotupas@gmail.com>
2022-10-17 14:19:54 -0700

    githubminich1K.Tupas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Kent Tupas
+// 10/17/2022
+// github mini challenge #1 "What is your name"
+// update and uploading work to github
+//
+
+Console.Clear();
+Console.WriteLine("__________________________________________________");
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+while(name != "end"){
+Console.WriteLine("Good Afternoon " + name);
+
+Console.WriteLine("How you been doing? ");
+Console.ReadLine();
+Console.WriteLine("Type end to stop or type in another name");
+name = Console.ReadLine();
+Console.WriteLine("__________________________________________________");
+}
